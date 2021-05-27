3689c657e512fe08c5dbe1ea4db8508d68c90564
Taras <tariksyurts@gmail.com>
2021-05-27 05:05:30 +0300

    main page 2/3

--- js/pages/main.js
+++ js/pages/main.js
@@ -1,10 +1,20 @@
 import {page} from '../components/page.js'
 
-function setupMainPage(){
-  const paragraph = document.createElement('p')
-  paragraph.insertAdjacentText('afterbegin', 'Hello world')
-  return page({content: { el: paragraph }})
-}
+// function setupMainPage(){
+//   const paragraph = document.createElement('p')
+//   paragraph.insertAdjacentText('afterbegin', 'Hello world')
+//   return page({content: { el: paragraph }})
+// }
 
-document.querySelector('.page').replaceChildren(...setupMainPage().elArray);
+// document.querySelector('.page').replaceChildren(...setupMainPage().elArray);
 
+$('.films-slider').slick({
+  infinite: true,
+  speed: 800,
+  fade: true,
+  prevArrow: false,
+  nextArrow: false,
+  autoplay: true,
+  autoplaySpeed: 4000,
+  cssEase: 'linear'
+});
